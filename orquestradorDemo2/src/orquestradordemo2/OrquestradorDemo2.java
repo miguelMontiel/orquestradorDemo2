1e05a28e80d1b67392b6e7908d9da0b76e3fe6bb
miguelMontiel <miguelmontielgalicia@hotmail.com>
2017-10-25 13:01:16 -0500

    Add files via upload

--- orquestradorDemo2/src/orquestradordemo2/OrquestradorDemo2.java
+++ orquestradorDemo2/src/orquestradordemo2/OrquestradorDemo2.java
@@ -3,6 +3,8 @@ package orquestradordemo2;
 import numeroPlaca.EventosnumeroPlaca;
 import numeroSerie.EventosnumeroSerie;
 import java.io.File;
+import java.util.Scanner;
+
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.bind.JAXBContext;
@@ -22,11 +24,13 @@ public class OrquestradorDemo2
     {
         try
         {            
-            JFileChooser jfilechooser = new JFileChooser();
+            JFileChooser jfilechooser = new JFileChooser("C:\\Users\\IBM_ADMIN\\Desktop\\orquestradorDemo2\\orquestradorDemo2\\src\\data");
             FileNameExtensionFilter filenameextensionfilter = new FileNameExtensionFilter("Archivos XML", "xml");
             jfilechooser.setFileFilter(filenameextensionfilter);
             int seleccionado = jfilechooser.showOpenDialog(null);
             String archivoSeleccionado = jfilechooser.getSelectedFile().getAbsolutePath();
+            
+            Scanner scanner = new Scanner(System.in);
 
             File file = new File(archivoSeleccionado);
 
@@ -39,140 +43,114 @@ public class OrquestradorDemo2
             Unmarshaller unmarshaller = jaxbcontext.createUnmarshaller();
             Siniestros siniestros = (Siniestros) unmarshaller.unmarshal(file);
             
-            Boolean h01 = false, h07 = false;
+            Boolean h01 = false, h07 = false, vehiculoTipoUso = false;
             String[] claveUsoVehiculo = {"", "Particular", "Privado", "", "Transporte Público", "Renta Diaria", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "Chofer Privado"};
             
-            if(siniestros.getTipoSiniestro().equals(""))
+            while(!h01 && !h07)
             {
-                if(siniestros.getTipoVehiculo().equals("AUT")  || siniestros.getTipoVehiculo().equals("CA1"))
+                System.out.println("¿Qué regla quiere buscar en este XML? (H01, H07 o Ambas)");
+                String reglaElegir = scanner.nextLine();
+                
+                if(reglaElegir.toLowerCase().equals("h01"))
                 {
-                    if(siniestros.getUsoVehiculo() == 01 || siniestros.getUsoVehiculo() == 02 || siniestros.getUsoVehiculo() == 04 || siniestros.getUsoVehiculo() == 05 ||siniestros.getUsoVehiculo() == 23)
-                    {
-                        System.out.println("Entra a H01! \n");
-                        System.out.println("Tipo de Vehiculo: " + siniestros.getTipoVehiculo());
-                        System.out.println("Uso del Vehiculo: " + siniestros.getUsoVehiculo() + " - " + claveUsoVehiculo[siniestros.getUsoVehiculo()] + "\n");
-
-                        h01 = true;
-                    }
+                	if(siniestros.getTipoVehiculo().equals("AUT")  || siniestros.getTipoVehiculo().equals("CA1") 
+                			&& siniestros.getUsoVehiculo() == 01 || siniestros.getUsoVehiculo() == 02 || siniestros.getUsoVehiculo() == 04 || siniestros.getUsoVehiculo() == 05 || siniestros.getUsoVehiculo() == 23)
+                	{
+	                	System.out.println("Entra a H01! \n");
+	                    System.out.println("Tipo de Vehiculo: " + siniestros.getTipoVehiculo());
+	                    System.out.println("Uso del Vehiculo: " + siniestros.getUsoVehiculo() + " - " + claveUsoVehiculo[siniestros.getUsoVehiculo()] + "\n");
+	                    h01 = true;
+	                    vehiculoTipoUso = true;
+                	}
+                }
+                else if(reglaElegir.toLowerCase().equals("h07"))
+                {
+                	System.out.println("Entra a H07! \n");
+                	System.out.println("Tipo de Siniestro: " + siniestros.getTipoSiniestro() + "\n");
+                	h07 = true;
+                }
+                else if(reglaElegir.toLowerCase().equals("ambas") || reglaElegir.toLowerCase().equals("ambos"))
+                {
+                	System.out.println("Entra a Ambas.");
+                	System.out.println("Tipo de Vehiculo: " + siniestros.getTipoVehiculo());
+                    System.out.println("Uso del Vehiculo: " + siniestros.getUsoVehiculo() + " - " + claveUsoVehiculo[siniestros.getUsoVehiculo()]);
+                    System.out.println("Tipo de Siniestro: " + siniestros.getTipoSiniestro() + "\n");
+                	h07 = true;
+                    h01 = true;
+                    vehiculoTipoUso = true;
+                }
+                else
+                {
+                	System.out.println("Sólo se pueden las reglas H01 y H07, o ambas. \n");
                 }
             }
-            else if(siniestros.getTipoSiniestro().toLowerCase().equals("robo"))
-            {
-            	System.out.println("Entra a H07! \n");
-            	System.out.println("Tipo de Siniestro: " + siniestros.getTipoSiniestro());
-            	
-            	h07 = true;
-            }
+            scanner.close();
             
             for(NumeroPlaca numeroplaca : siniestros.getNumeroPlaca())
             {
                 for(EventosnumeroPlaca eventosnumeroplaca : numeroplaca.getEventos_numeroPlaca())
                 {
-                    if(h01 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H01") && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 4)
-                    {
-                        System.out.println("¡¡¡La placa '" + numeroplaca.getId_numeroPlaca() + "' tiene '" + eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() + "' eventos en la regla '" + eventosnumeroplaca.getEventosReglasID_numeroPlaca() + "'!!!");
-                    }
-                    else if(h01 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H01b") && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 4)
-                    {
-                        System.out.println("¡¡¡La placa '" + numeroplaca.getId_numeroPlaca() + "' tiene '" + eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() + "' eventos en la regla '" + eventosnumeroplaca.getEventosReglasID_numeroPlaca() + "'!!!");
-                    }
-                    else if(h01 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H01c") && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 5)
-                    {
-                        System.out.println("¡¡¡La placa '" + numeroplaca.getId_numeroPlaca() + "' tiene '" + eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() + "' eventos en la regla '" + eventosnumeroplaca.getEventosReglasID_numeroPlaca() + "'!!!");
-                    }
-                    else if(h07 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H07") && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 1)
-                    {
-                        System.out.println("¡¡¡La placa '" + numeroplaca.getId_numeroPlaca() + "' tiene '" + eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() + "' eventos en la regla '" + eventosnumeroplaca.getEventosReglasID_numeroPlaca() + "'!!!");
-                    }
-                    else
-                    {
-                        System.out.println("Todo chido con el número de Placa.");
-                        break;
-                    }
+                	if(h01 && vehiculoTipoUso && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 4 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H01") 
+                			|| h01 && vehiculoTipoUso && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 4 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H01b")
+                			|| h01 && vehiculoTipoUso && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 5 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H01c")
+                			|| h07 && eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() > 1 && eventosnumeroplaca.getEventosReglasID_numeroPlaca().equals("H07"))
+                	{
+                		System.out.println("La Placa: '" + numeroplaca.getId_numeroPlaca() + "' tiene " + eventosnumeroplaca.getEventosReglasTotales_numeroPlaca() + 
+                				" eventos con la regla '" + eventosnumeroplaca.getEventosReglasID_numeroPlaca() + "'.");
+                	}
                 }
+                System.out.println("No hay más problemas con el número de Placa: " + numeroplaca.getId_numeroPlaca() + "\n");
             }
-            
-            System.out.println();
-            
+
             for(NumeroSerie numeroserie : siniestros.getNumeroSerie())
             {                
                 for(EventosnumeroSerie eventosnumeroserie : numeroserie.getEventos_numeroSerie())
                 {
-                    if(h01 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H01") && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 4)
-                    {
-                        System.out.println("¡¡¡El número de Serie '" + numeroserie.getId_numeroSerie() + "' tiene '" + eventosnumeroserie.getEventosReglasTotales_numeroSerie() + "' eventos en la regla '" + eventosnumeroserie.getEventosReglasID_numeroSerie() + "'!!!");
-                    }
-                    else if(h01 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H01b") && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 4)
-                    {
-                        System.out.println("¡¡¡El número de Serie '" + numeroserie.getId_numeroSerie() + "' tiene '" + eventosnumeroserie.getEventosReglasTotales_numeroSerie() + "' eventos en la regla '" + eventosnumeroserie.getEventosReglasID_numeroSerie() + "'!!!");
-                    }
-                    else if(h01 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H01c") && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 5)
-                    {
-                        System.out.println("¡¡¡El número de Serie '" + numeroserie.getId_numeroSerie() + "' tiene '" + eventosnumeroserie.getEventosReglasTotales_numeroSerie() + "' eventos en la regla '" + eventosnumeroserie.getEventosReglasID_numeroSerie() + "'!!!");
-                    }
-                    else if(h07 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H07") && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 1)
-                    {
-                        System.out.println("¡¡¡El número de Serie '" + numeroserie.getId_numeroSerie() + "' tiene '" + eventosnumeroserie.getEventosReglasTotales_numeroSerie() + "' eventos en la regla '" + eventosnumeroserie.getEventosReglasID_numeroSerie() + "'!!!");
-                    }
-                    else
-                    {
-                        System.out.println("Todo chido con el número de Serie.");
-                        break;
-                    }
+                	if(h01 && vehiculoTipoUso && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 4 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H01") 
+                			|| h01 && vehiculoTipoUso && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 4 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H01b")
+                			|| h01 && vehiculoTipoUso && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 5 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H01c")
+                			|| h07 && eventosnumeroserie.getEventosReglasTotales_numeroSerie() > 1 && eventosnumeroserie.getEventosReglasID_numeroSerie().equals("H07"))
+                	{
+                		System.out.println("El número de Serie: '" + numeroserie.getId_numeroSerie() + "' tiene " + eventosnumeroserie.getEventosReglasTotales_numeroSerie() + 
+                				" eventos con la regla '" + eventosnumeroserie.getEventosReglasID_numeroSerie() + "'.");
+                	}
                 }
+                System.out.println("No hay más problemas con el número de Serie: " + numeroserie.getId_numeroSerie() + "\n");
             }
-            
-            System.out.println();
             
             for(RFCContratante rfccontratante : siniestros.getRfcContratante())
             {
                 for(EventosrfcContratante eventosrfccontratante : rfccontratante.getEventosrfccontratente())
                 {
-                    if(h01 && eventosrfccontratante.getEventosReglasID_rfcContratante().equals("H01") && eventosrfccontratante.getEventosReglasTotales_rfcContratante() > 4)
+                    if(h01 && vehiculoTipoUso && eventosrfccontratante.getEventosReglasTotales_rfcContratante() > 4 && eventosrfccontratante.getEventosReglasID_rfcContratante().equals("H01") 
+                    		|| h01 && vehiculoTipoUso && eventosrfccontratante.getEventosReglasTotales_rfcContratante() > 4 && eventosrfccontratante.getEventosReglasID_rfcContratante().equals("H01b")
+                    		|| h01 && vehiculoTipoUso && eventosrfccontratante.getEventosReglasTotales_rfcContratante() > 5 && eventosrfccontratante.getEventosReglasID_rfcContratante().equals("H01c"))
                     {
-                        System.out.println("¡¡¡El RFC del Contratante '" + rfccontratante.getId_rfcContratante() + "' tiene '" + eventosrfccontratante.getEventosReglasTotales_rfcContratante() + "' eventos en la regla '" + eventosrfccontratante.getEventosReglasID_rfcContratante() + "'!!!");
-                    }
-                    else if(h01 && eventosrfccontratante.getEventosReglasID_rfcContratante().equals("H01b") && eventosrfccontratante.getEventosReglasTotales_rfcContratante() > 4)
-                    {
-                        System.out.println("¡¡¡El RFC del Contratante '" + rfccontratante.getId_rfcContratante() + "' tiene '" + eventosrfccontratante.getEventosReglasTotales_rfcContratante() + "' eventos en la regla '" + eventosrfccontratante.getEventosReglasID_rfcContratante() + "'!!!");
-                    }
-                    else if(h01 && eventosrfccontratante.getEventosReglasID_rfcContratante().equals("H01c") && eventosrfccontratante.getEventosReglasTotales_rfcContratante() > 5)
-                    {
-                        System.out.println("¡¡¡El RFC del Contratante '" + rfccontratante.getId_rfcContratante() + "' tiene '" + eventosrfccontratante.getEventosReglasTotales_rfcContratante() + "' eventos en la regla '" + eventosrfccontratante.getEventosReglasID_rfcContratante() + "'!!!");
-                    }
-                    else if(h07) {}
-                    else
-                    {
-                        System.out.println("Todo chido con el RFC del Contratante.");
-                        break;
+                        System.out.println("El RFC del Contratante '" + rfccontratante.getId_rfcContratante() + "' tiene " + eventosrfccontratante.getEventosReglasTotales_rfcContratante() + 
+                        		" eventos con la regla '" + eventosrfccontratante.getEventosReglasID_rfcContratante() + "'.");
                     }
                 }
+                if(h01)
+                {
+                	System.out.println("No hay más problemas con el RFC del Conductor: " + rfccontratante.getId_rfcContratante() + "\n");
+                }
             }
-            
-            System.out.println();
-            
+
             for(RFCConductor rfcconductor : siniestros.getRfcConductor())
             {
                 for(EventosrfcConductor eventosrfcconductor : rfcconductor.getEventosrfcconductor())
                 {
-                    if(h01 && eventosrfcconductor.getEventosReglasID_rfcConductor().equals("H01") && eventosrfcconductor.getEventosReglasTotales_rfcConductor() > 4)
+                    if(h01 && vehiculoTipoUso && eventosrfcconductor.getEventosReglasTotales_rfcConductor() > 4 && eventosrfcconductor.getEventosReglasID_rfcConductor().equals("H01")
+                    		|| h01 && vehiculoTipoUso && eventosrfcconductor.getEventosReglasTotales_rfcConductor() > 4 && eventosrfcconductor.getEventosReglasID_rfcConductor().equals("H01b")
+                    		|| h01 && vehiculoTipoUso && eventosrfcconductor.getEventosReglasTotales_rfcConductor() > 5 && eventosrfcconductor.getEventosReglasID_rfcConductor().equals("H01c"))
                     {
-                        System.out.println("¡¡¡El RFC del Contratante '" + rfcconductor.getId_rfcConductor() + "' tiene '" + eventosrfcconductor.getEventosReglasTotales_rfcConductor() + "' eventos en la regla '" + eventosrfcconductor.getEventosReglasID_rfcConductor() + "'!!!");
+                        System.out.println("El RFC del Conductor '" + rfcconductor.getId_rfcConductor() + "' tiene " + eventosrfcconductor.getEventosReglasTotales_rfcConductor() + 
+                        		" eventos con la regla '" + eventosrfcconductor.getEventosReglasID_rfcConductor() + "'.");
                     }
-                    else if(h01 && eventosrfcconductor.getEventosReglasID_rfcConductor().equals("H01b") && eventosrfcconductor.getEventosReglasTotales_rfcConductor() > 4)
-                    {
-                        System.out.println("¡¡¡El RFC del Contratante '" + rfcconductor.getId_rfcConductor() + "' tiene '" + eventosrfcconductor.getEventosReglasTotales_rfcConductor() + "' eventos en la regla '" + eventosrfcconductor.getEventosReglasID_rfcConductor() + "'!!!");
-                    }
-                    else if(h01 && eventosrfcconductor.getEventosReglasID_rfcConductor().equals("H01c") && eventosrfcconductor.getEventosReglasTotales_rfcConductor() > 5)
-                    {
-                        System.out.println("¡¡¡El RFC del Contratante '" + rfcconductor.getId_rfcConductor() + "' tiene '" + eventosrfcconductor.getEventosReglasTotales_rfcConductor() + "' eventos en la regla '" + eventosrfcconductor.getEventosReglasID_rfcConductor() + "'!!!");
-                    }
-                    else if(h07) { }
-                    else
-                    {
-                        System.out.println("Todo chido con el RFC del Conductor.");
-                        break;
-                    }
+                }
+                if(h01)
+                {
+                	System.out.println("No hay más problemas con el RFC del Conductor: " + rfcconductor.getId_rfcConductor() + "\n");
                 }
             }
         }
